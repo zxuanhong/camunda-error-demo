a4b0af70cebe9a7b95db5df318b3c3dfa9fcbf5b
zxuanhong <z7630853@163.com>
2023-11-05 10:36:31 +0800

    init

---
+++ src/main/java/com/example/demo/config/ResourceServerConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
+public class ResourceServerConfig {
+
+    @Bean
+    public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
+        // 禁用csrf与cors
+        http.csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/url/test").permitAll();
+                }
+        );
+        return http.build();
+    }
+
+}
+
